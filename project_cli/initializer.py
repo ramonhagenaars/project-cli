45518664d704119cc1d4f24bdba1e80b50904d98
Ramon <r.amonhagenaars@gmail.com>
2018-12-26 16:50:03 +0100

    Added option to define the output dir.

--- project_cli/initializer.py
+++ project_cli/initializer.py
@@ -16,6 +16,7 @@ _DEFAULT_VERSION = '0.1.0'
 _DEFAULT_AUTHOR = os.getenv(ENV_USER)
 _DEFAULT_RUNNABLE = 'yes'
 _DEFAULT_CORRECT = 'yes'
+_DEFAULT_OUTPUT_DIR = './'
 
 try:
     input_ = raw_input  # Python2.7
@@ -24,22 +25,23 @@ except NameError:
 
 
 def setup(cmd_args):
-    defaults = (_DEFAULT_NAME, _DEFAULT_DESCRIPTION, _DEFAULT_VERSION,
-                _DEFAULT_AUTHOR, _DEFAULT_RUNNABLE)
+    defaults = (_DEFAULT_NAME, _DEFAULT_OUTPUT_DIR, _DEFAULT_DESCRIPTION,
+                _DEFAULT_VERSION, _DEFAULT_AUTHOR, _DEFAULT_RUNNABLE)
     inputs = defaults if cmd_args.yes else _get_user_inputs()
     _do_setup(*inputs)
 
 
-def _do_setup(name, description, version, author, runnable):
-    main_package = name
-    tests_package = 'tests'
+def _do_setup(name, output_dir, description, version, author, runnable):
+    main_package = os.path.join(output_dir, name)
+    tests_package = os.path.join(output_dir, 'tests')
 
     os.makedirs(main_package, exist_ok=True)
     os.makedirs(tests_package, exist_ok=True)
 
-    _create_file('README.rst', get_readme(name, description, runnable))
+    _create_file('README.rst', get_readme(name, description, runnable),
+                 output_dir)
     _create_file('setup.py', get_setup(name, version, author, description,
-                                       runnable))
+                                       runnable), output_dir)
     _create_file('__init__.py', get_init(), main_package)
     _create_file('__main__.py', get_main(name), main_package)
     _create_file('__init__.py', get_init(), tests_package)
@@ -64,21 +66,17 @@ def _get_input(message, default, regex=None, requirements=None):
 
 
 def _get_user_inputs():
-
     name = _get_input('Project name', _DEFAULT_NAME, '[a-zA-Z_]\w*',
                       'A project name must be a word; it must start with a ' +\
                       'letter or underscore and may contain only letters, ' +\
                       'underscores and numbers in the remainder.')
-
+    output_dir = _get_input('Output directory', './')  # TODO: validate it!
     description = _get_input('Description', 'A Python %s' % name)
-
     version = _get_input('Initial version', _DEFAULT_VERSION,
                          '[0-9]\.[0-9]\.[0-9]',
                          'A version must have the form: x.y.z where x, y ' +\
                          'and z are all whole numbers.')
-
     author = _get_input('Author', _DEFAULT_AUTHOR)
-
     runnable = _is_yes(_get_input('Is the project runnable?',
                                   _DEFAULT_RUNNABLE))
 
@@ -86,6 +84,7 @@ def _get_user_inputs():
     print('Setup overview')
     print('--------------')
     print('Project name:    %s' % name)
+    print('Output dir:      %s' % os.path.abspath(output_dir))
     print('Description:     %s' % description)
     print('Initial version: %s' % version)
     print('Author:          %s' % author)
@@ -95,10 +94,10 @@ def _get_user_inputs():
     if not correct:
         print()
         return _get_user_inputs()
-    return name, description, version, author, runnable
+    return name, output_dir, description, version, author, runnable
 
 
 def _create_file(fname, content, pname=None):
-    fpath = '%s/%s' % (pname, fname) if pname else fname
-    with open(fpath, 'w+') as f:
+    output_path = os.path.abspath(os.path.join(pname, fname))
+    with open(output_path, 'w+') as f:
         f.write(content)
